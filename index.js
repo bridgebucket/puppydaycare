031d6ad5aca4750eb45a9404caf4a0eedd1493f7
bridgebucket <bridgebucket@gmail.com>
2023-07-26 21:04:07 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
 }
 
 function copyText() {
-  navigator.clipboard.writeText
-  ("mc.puppydaycare.xyz");
+  navigator.clipboard.writeText
+  ("mc.puppydaycare.xyz");
 
-  var popup = document.getElementById("popup"); 
-  popup.classList.toggle("show");
+  var popup = document.getElementById("popup"); 
+  popup.classList.toggle("show");
 }
